a6c645f98ec0efcfdc949f09b5a4817fccb35a0e
laflosh <l_florent_l@hotmail.fr>
2025-02-02 20:11:15 +0100

    init springboot project for refactoring original air business project

---
+++ src/test/java/com/airbusiness/airbusiness_mvc/AirbusinessMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbusiness.airbusiness_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbusinessMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
